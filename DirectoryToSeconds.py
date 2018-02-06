31803c97caa00ee296e04fc711ace22530d4f4d5
Tumo Masire <tumomasire@gmail.com>
2018-02-06 09:03:50 +0000

    Clearing up the output generated.

--- DirectoryToSeconds.py
+++ DirectoryToSeconds.py
@@ -1,23 +1,17 @@
 #! /usr/bin/env python3
 #Tumo Masire 05 Feb 2018
 
-def hello(a,b,c):
-    print ("hello and that's your sum:")
-    sum=int(a)+int(b)+int(c)
-    print (sum)
-    
 def getLength(file):
+    #ToDo: extend to support more file types, e.g .mov
     tag = TinyTag.get(file)
     return tag.duration
     
-
 def duration(in_user):
     answer_secs = 0
     for root, directories, filenames in os.walk(in_user):
         for filename in filenames:
             full_path = os.path.join(root,filename)
-            if filename.endswith(".mp4"):
-                #print("{}".format(full_path))
+            if filename.endswith((".mp4","mov")):
                 answer_secs += getLength(full_path)
     return answer_secs
                 
@@ -25,7 +19,6 @@ def to_string(s):
     string = ("{}:{}:{}".format(int(s//3600), int(s%3600//60), int(s%60)))
     return string
             
-
 if __name__ == "__main__":
     import sys
     import os
@@ -34,10 +27,13 @@ if __name__ == "__main__":
     
     answer_secs = 0
     user_directs = arguments = sys.argv[1:]
+    print("Time Format - hh:mm:ss")
     
     for i in range(len(user_directs)):
-        print(i)
         direcs = user_directs[i]
-        answer_secs += (duration(direcs))
+        seconds = duration(direcs)
+        print("file {} - {}".format(i+1, to_string(seconds)))
+        answer_secs += seconds
+        
     ANSWER = to_string(answer_secs)
-    print(ANSWER)
+    print("Total: {}".format(ANSWER))
