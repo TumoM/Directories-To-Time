e59bb1659e9d36490cbf81d5ee32a5d3068543f3
Tumo Masire <tumomasire@gmail.com>
2018-02-06 11:30:57 +0200

    More (UX) tweaks

--- DirectoryToSeconds.py
+++ DirectoryToSeconds.py
@@ -1,19 +1,19 @@
 #! /usr/bin/env python3
 #Tumo Masire 05 Feb 2018
 
-def getLength(file):
+def getLength(my_file):
     #ToDo: extend to support more file types, e.g .mov
-    tag = TinyTag.get(file)
+    tag = TinyTag.get(my_file)
     return tag.duration
     
 def duration(in_user):
-    answer_secs = 0
+    loc_answer_secs = 0
     for root, directories, filenames in os.walk(in_user):
         for filename in filenames:
             full_path = os.path.join(root,filename)
             if filename.endswith((".mp4","mov")):
-                answer_secs += getLength(full_path)
-    return answer_secs
+                loc_answer_secs += getLength(full_path)
+    return loc_answer_secs
                 
 def to_string(s):
     string = ("{}:{}:{}".format(int(s//3600), int(s%3600//60), int(s%60)))
@@ -22,17 +22,18 @@ def to_string(s):
 if __name__ == "__main__":
     import sys
     import os
-    import datetime
     from tinytag import TinyTag
     
     answer_secs = 0
     user_directs = arguments = sys.argv[1:]
+    clear = lambda: os.system('cls')
+    clear()
     print("Time Format - hh:mm:ss")
     
     for i in range(len(user_directs)):
         direcs = user_directs[i]
         seconds = duration(direcs)
-        print("file {} - {}".format(i+1, to_string(seconds)))
+        print("Folder {} - {}".format(i+1, to_string(seconds)))
         answer_secs += seconds
         
     ANSWER = to_string(answer_secs)
